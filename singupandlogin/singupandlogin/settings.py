0bc8c4f4c63db4a9a940ca5372d39df6dbe36305
sujon <sujonict07@gmail.com>
2018-09-19 16:44:57 +0600

    Login from update

--- singupandlogin/singupandlogin/settings.py
+++ singupandlogin/singupandlogin/settings.py
@@ -116,5 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "cloud_assets", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "cloud_assets", "assets"),)
