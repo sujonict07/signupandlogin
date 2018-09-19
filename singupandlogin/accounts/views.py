0489cfef6d91bb6866f4426f802f263cc2c99981
sujon <sujonict07@gmail.com>
2018-09-19 18:29:04 +0600

    registration from added

---
+++ singupandlogin/accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'singup.html'
--- singupandlogin/singupandlogin/urls.py
+++ singupandlogin/singupandlogin/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path("", include("django.contrib.auth.urls")),
+    path("accounts/",include('accounts.urls')),
 ]
