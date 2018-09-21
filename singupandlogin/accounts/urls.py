c249210a85c8d759b69a0c63c5e1acbe6c7382b6
sujonict07 <sujonict07@gmail.com>
2018-09-21 22:00:07 +0600

    Registration form added

--- singupandlogin/accounts/urls.py
+++ singupandlogin/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import SignUp
+from .views import SignUp, signup
 
 
 urlpatterns = [
-    path('signup/', SignUp.as_view(), name='signup'),
+    path('signup/', signup, name='signup'),
 ]
--- singupandlogin/accounts/views.py
+++ singupandlogin/accounts/views.py
@@ -3,10 +3,44 @@ from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+from .forms import SignUpForm
+
 # Create your views here.
 
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'singup.html'
+    template_name = 'signup.html'
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        print("Data {}".format(form.cleaned_data()))
+        if form.is_valid():
+
+            form.save()
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'signup.html', {'form':form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
